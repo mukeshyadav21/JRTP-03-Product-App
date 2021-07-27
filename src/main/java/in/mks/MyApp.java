823201e783a535cfd50b9a5cbd13105d9486d864
Mukesh <abc@example.com>
2021-07-27 10:00:41 +0530

    added int var

--- src/main/java/in/mks/MyApp.java
+++ src/main/java/in/mks/MyApp.java
@@ -5,6 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
      System.out.println("Welcome to MyApp product is available here");
      System.out.println("Modification in product in here");
+     int i=10;
 	}
 
 }
